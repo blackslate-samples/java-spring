a974d096030e416fe91b93dedeeb9dad71abd0cf
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2023-12-06 18:34:51 +0530

    Added the project files of Spring Boot Thymeleaf CRUD

---
+++ SpringBoot-ThymeleafCrudTutorial/src/main/java/com/application/thymeleafcrud/SpringBootThymeleafCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.application.thymeleafcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafCrudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafCrudTutorialApplication.class, args);
+	}
+
+}
